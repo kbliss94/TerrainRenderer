2fc3c510a75bc88b36621c26770092460334d103
kbliss94 <Kaitlyn.Bliss@ucf.edu>
2017-05-24 18:19:00 -0400

    Project currently renders a black screen with no borders

--- TerrainRenderer/TerrainRenderer/Main.cpp
+++ TerrainRenderer/TerrainRenderer/Main.cpp
@@ -1,33 +1,31 @@
 #include "System.h"
 
 using namespace std;
+using namespace TerrainRenderer;
 
 //!This terrain renderer will procedurally texture and render a large-scale terrain.
 /*!*/
 
-namespace TerrainRenderer
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+	System* system;
+	bool result;
+
+	system = new System;
+	if (!system)
 	{
-		System* system;
-		bool result;
-
-		system = new System;
-		if (!system)
-		{
-			return 0;
-		}
-
-		result = system->Initialize();
-		if (result)
-		{
-			system->Run();
-		}
-
-		system->Shutdown();
-		delete system;
-		system = nullptr;
-
 		return 0;
 	}
+
+	result = system->Initialize();
+	if (result)
+	{
+		system->Run();
+	}
+
+	system->Shutdown();
+	delete system;
+	system = nullptr;
+
+	return 0;
 }
